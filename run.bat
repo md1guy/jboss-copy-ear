6bc7f499a3a9d0527f4339a1974ce3ea26f92d6d
ovolc <ovolc@softserveinc.com>
2019-11-27 14:53:35 +0200

    changed interpreter to sh.exe

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-cmd /c "C:\Program Files\Git\bin\bash.exe" --login -i -- C:\Users\ovolc\Desktop\home\repos\jboss-copy-ear\cpear.sh
+cmd /c "C:\Program Files\Git\bin\sh.exe" --login -i -- C:\Users\ovolc\Desktop\home\repos\jboss-copy-ear\cpear.sh
